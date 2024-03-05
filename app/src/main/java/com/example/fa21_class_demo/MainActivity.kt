848c25041b156397a4fac1bd2f96a8549f99d212
Ali Usman <ali.usman32332@gmail.com>
2024-03-05 15:00:06 +0500

    some code refoctroing before closing

--- app/src/main/java/com/example/fa21_class_demo/MainActivity.kt
+++ app/src/main/java/com/example/fa21_class_demo/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.fa21_class_demo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.databinding.DataBindingUtil
 import com.example.fa21_class_demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var dataBinding: ActivityMainBinding
-    var player: Boolean = true
+    private lateinit var dataBinding: ActivityMainBinding
+    private var player: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
@@ -16,25 +17,25 @@ class MainActivity : AppCompatActivity() {
 
         dataBinding.first.setOnClickListener {
             if (player) {
-                dataBinding.firstchild.background = getDrawable(R.drawable.ic_baseline_check_24)
+                dataBinding.firstchild.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_check_24)
             } else {
-                dataBinding.firstchild.background = getDrawable(R.drawable.ic_baseline_clear_24)
+                dataBinding.firstchild.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_clear_24)
             }
             switchPlayer()
         }
 
         dataBinding.second.setOnClickListener {
             if (player) {
-                dataBinding.secondChild.background = getDrawable(R.drawable.ic_baseline_check_24)
+                dataBinding.secondChild.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_check_24)
             } else {
-                dataBinding.secondChild.background = getDrawable(R.drawable.ic_baseline_clear_24)
+                dataBinding.secondChild.background = AppCompatResources.getDrawable(this, R.drawable.ic_baseline_clear_24)
             }
             switchPlayer()
         }
 
     }
 
-    fun switchPlayer() {
+    private fun switchPlayer() {
         player = !player
     }
 }
