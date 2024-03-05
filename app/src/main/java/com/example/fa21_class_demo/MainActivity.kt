3db64e03bf3b65577f15a3a4a1715affe5ecb71f
Ali Usman <ali.usman32332@gmail.com>
2024-03-05 12:38:19 +0500

    working on ticktack game

--- app/src/main/java/com/example/fa21_class_demo/MainActivity.kt
+++ app/src/main/java/com/example/fa21_class_demo/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.fa21_class_demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.fa21_class_demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var dataBinding: ActivityMainBinding
+    var player: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        dataBinding.first.setOnClickListener {
+            if (player) {
+                dataBinding.firstchild.background = getDrawable(R.drawable.ic_baseline_check_24)
+            } else {
+                dataBinding.firstchild.background = getDrawable(R.drawable.ic_baseline_clear_24)
+            }
+            switchPlayer()
+        }
+
+        dataBinding.second.setOnClickListener {
+            if (player) {
+                dataBinding.secondChild.background = getDrawable(R.drawable.ic_baseline_check_24)
+            } else {
+                dataBinding.secondChild.background = getDrawable(R.drawable.ic_baseline_clear_24)
+            }
+            switchPlayer()
+        }
+
+    }
+
+    fun switchPlayer() {
+        player = !player
     }
 }
