6a1ce5cc71a1793b71ac5416c8b7bee0160541ef
Ali Usman <ali.usman32332@gmail.com>
2024-03-12 10:46:21 +0500

    game working

--- app/src/main/java/com/example/fa21_class_demo/MainActivity.kt
+++ app/src/main/java/com/example/fa21_class_demo/MainActivity.kt
@@ -1,81 +1,177 @@
 package com.example.fa21_class_demo
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import com.example.fa21_class_demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var dataBinding: ActivityMainBinding
-    private var player: Boolean = false
 
-    private val entertainedComponents: MutableList<Int> = arrayListOf()
+    data class PlayerTurnModel(
+        val player: Boolean,
+        val id: Int
+    )
+    private lateinit var dataBinding: ActivityMainBinding
+    private var player: Boolean = true
+
+    private val entertainedComponents: MutableList<PlayerTurnModel> = arrayListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
 
-        dataBinding.center.setOnClickListener {
-            if(!entertainedComponents.contains(R.id.center)) {
-                if (player) {
-                    dataBinding.centerchild
-                        .background =
-                        AppCompatResources.getDrawable(this, R.drawable.ic_baseline_check_24)
-                } else {
-                    dataBinding.centerchild.background =
-                        AppCompatResources.getDrawable(this, R.drawable.ic_baseline_clear_24)
-                }
-                switchPlayer()
-                entertainedComponents.add(R.id.center)
-            }
-        }
-
-        dataBinding.middleLeft.setOnClickListener {
-            if(!entertainedComponents.contains(R.id.middle_left)) {
-
-                if (player) {
-                    dataBinding.middleLeftChild.background =
-                        AppCompatResources.getDrawable(this, R.drawable.ic_baseline_check_24)
-                } else {
-                    dataBinding.middleLeftChild.background =
-                        AppCompatResources.getDrawable(this, R.drawable.ic_baseline_clear_24)
-                }
-                switchPlayer()
-                entertainedComponents.add(R.id.middle_left)
-            }
-
-        }
-
-        dataBinding.middleRight.setOnClickListener {
-            if (player) {
-                dataBinding.middleRightChild.background =
-                    AppCompatResources.getDrawable(this, R.drawable.ic_baseline_check_24)
-            } else {
-                dataBinding.middleRightChild.background =
-                    AppCompatResources.getDrawable(this, R.drawable.ic_baseline_clear_24)
-            }
-            switchPlayer()
-            entertainedComponents.add(R.id.middle_right)
-
-        }
         dataBinding.topLeft.setOnClickListener {
+                handlePlayerInput(dataBinding.topLeftChild)
+        }
+        dataBinding.topMiddle.setOnClickListener {
+            handlePlayerInput(dataBinding.topMiddleChild)
+        }
+        dataBinding.topRight.setOnClickListener {
+            handlePlayerInput(dataBinding.topRightChild)
+        }
+        dataBinding.middleLeft.setOnClickListener {
+            handlePlayerInput(dataBinding.middleLeftChild)
+        }
+        dataBinding.center.setOnClickListener {
+            handlePlayerInput(dataBinding.centerchild)
+        }
+        dataBinding.middleRight.setOnClickListener {
+            handlePlayerInput(dataBinding.middleRightChild)
+        }
+        dataBinding.bottomLeft.setOnClickListener {
+            handlePlayerInput(dataBinding.bottomLeftChild)
+        }
+        dataBinding.bottomCenter.setOnClickListener {
+            handlePlayerInput(dataBinding.bottomCenterChild)
+        }
+        dataBinding.bottomRight.setOnClickListener {
+            handlePlayerInput(dataBinding.bottomRightChild)
+        }
+    }
+
+
+    private fun handlePlayerInput(view: ConstraintLayout) {
+        if(entertainedComponents.filter { it.id == view.id }.size == 0) {
             if (player) {
-                dataBinding.topLeftChild.background =
+                view
+                    .background =
                     AppCompatResources.getDrawable(this, R.drawable.ic_baseline_check_24)
             } else {
-                dataBinding.topLeftChild.background =
+                view.background =
                     AppCompatResources.getDrawable(this, R.drawable.ic_baseline_clear_24)
             }
+            entertainedComponents.add(PlayerTurnModel(player = player, id = view.id))
             switchPlayer()
-            entertainedComponents.add(R.id.top_left)
-
         }
 
+        val winState = checkWinState()
+        var winner = ""
+        if(winState == true) {
+            winner = "One"
+        } else {
+            winner = "Two"
+        }
+       if(winState==null) {
+           Toast.makeText(this, "No win yet",Toast.LENGTH_SHORT).show()
+       } else {
+           Toast.makeText(this, "Player $winner Wins",Toast.LENGTH_SHORT).show()
+       }
     }
+
+
+
 
     private fun switchPlayer() {
         player = !player
+    }
+
+
+    private fun checkWinState(): Boolean? {
+        Log.e("TAG", "checkWinState: entertainedComponents ${entertainedComponents}", )
+        if(entertainedComponents.size > 4) {
+            try {
+                if(entertainedComponents.filter { it.id == R.id.top_left_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.top_middle_Child }.get(0).player && entertainedComponents.filter { it.id == R.id.top_left_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.top_right_Child }.get(0).player)
+                {
+                    dataBinding.topLine.visibility = View.VISIBLE
+                    return entertainedComponents.filter { it.id == R.id.top_left_Child }.get(0).player
+                }
+            }catch (err:Exception) {
+
+            }
+
+            try {
+                if(entertainedComponents.filter { it.id == R.id.middle_left_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player && entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player == entertainedComponents.filter { it.id == R.id.middle_right_Child }.get(0).player)
+                {
+                    dataBinding.middleLine.visibility = View.VISIBLE
+
+                    return entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player
+                }
+            } catch (error:Exception) {
+
+            }
+
+
+            try {
+                if(entertainedComponents.filter { it.id == R.id.bottom_left_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.bottom_centerChild }.get(0).player && entertainedComponents.filter { it.id == R.id.bottom_centerChild }.get(0).player == entertainedComponents.filter { it.id == R.id.bottom_right_Child }.get(0).player)
+                {
+                    dataBinding.bottomLine.visibility = View.VISIBLE
+
+                    return entertainedComponents.filter { it.id == R.id.bottom_left_Child }.get(0).player
+                }
+            }catch (error:Exception) {
+
+            }
+             // Columns
+
+            try {
+                if(entertainedComponents.filter { it.id == R.id.top_left_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.middle_left_Child }.get(0).player && entertainedComponents.filter { it.id == R.id.middle_left_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.bottom_left_Child }.get(0).player)
+                {
+                    return entertainedComponents.filter { it.id == R.id.middle_left_Child }.get(0).player
+                }
+            }catch (err:Exception) {
+
+            }
+
+            try {
+                if(entertainedComponents.filter { it.id == R.id.top_middle_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player && entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player == entertainedComponents.filter { it.id == R.id.bottom_centerChild }.get(0).player)
+                {
+                    return entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player
+                }
+            } catch (error:Exception) {
+
+            }
+
+
+            try {
+                if(entertainedComponents.filter { it.id == R.id.top_right_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.middle_right_Child }.get(0).player && entertainedComponents.filter { it.id == R.id.middle_right_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.bottom_right_Child }.get(0).player)
+                {
+                    return entertainedComponents.filter { it.id == R.id.middle_right_Child }.get(0).player
+                }
+            }catch (error:Exception) {
+
+            }
+
+            // Diagnal
+
+
+            try {
+                if(entertainedComponents.filter { it.id == R.id.top_right_Child }.get(0).player == entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player && entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player == entertainedComponents.filter { it.id == R.id.bottom_left_Child }.get(0).player)
+                {
+                    dataBinding.dRightToLeft.visibility = View.VISIBLE
+                    return entertainedComponents.filter { it.id == R.id.centerchild }.get(0).player
+                }
+            }catch (error:Exception) {
+
+            }
+
+        }
+
+        return null
     }
 }
